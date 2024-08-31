c57b95c43c974a2a4d744d6686b0a73a3388e2f0
Eric <e_chia0921@outlook.com>
2024-09-01 00:39:24 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Hero from './hero'
+import Projects from './Projects'
+
+const App = () => {
+  return (
+    <main>
+      <Hero />
+      <Projects />
+    </main>
+  )
+}
+export default App
